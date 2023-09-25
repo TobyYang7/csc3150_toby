a701e56eeb47e6f2a00719b47f56d439c5533ab1
TobyYang7 <tobyyang7@foxmail.com>
2023-09-25 08:34:49 +0000

    1

--- Tut2/example_codes/example1.c
+++ Tut2/example_codes/example1.c
@@ -4,23 +4,29 @@
 #include <string.h>
 #include <sys/types.h>
 
-int main(int argc, char*argv[]){
+// This is the main function that takes in command line arguments.
+int main(int argc, char *argv[])
+{
     pid_t pid;
     printf("Start to fork\n");
-    pid = fork();
-    if (pid == -1){
-        perror("error");
-        exit(1);
+    pid = fork(); // Create a new process by duplicating the calling process.
+    if (pid == -1)
+    {                    // If fork() returns -1, an error occurred.
+        perror("error"); // Print an error message.
+        exit(1);         // Exit the program with a status of 1.
     }
-    else{
-        if (pid == 0){
-            printf("Child process with return value %d\n",pid);
-            exit(0);
+    else
+    {
+        if (pid == 0)
+        {                                                        // If fork() returns 0, this is the child process.
+            printf("Child process with return value %d\n", pid); // Print a message indicating that this is the child process.
+            exit(0);                                             // Exit the program with a status of 0.
         }
-        else{
-            printf("Parent process with return value %d\n",pid);
-            exit(0);
+        else
+        {                                                         // If fork() returns a positive value, this is the parent process.
+            printf("Parent process with return value %d\n", pid); // Print a message indicating that this is the parent process.
+            exit(0);                                              // Exit the program with a status of 0.
         }
     }
-    return 0;
+    return 0; // Return 0 to indicate successful program execution.
 }
--- Tut2/example_codes/test.c
+++ Tut2/example_codes/test.c
@@ -4,7 +4,48 @@
 #include <string.h>
 #include <sys/types.h>
 
-int main(void) {
-    printf("Test program pid:%d\n",getpid());
+// int main(void)
+// {
+//     printf("Test program pid:%d\n", getpid());
+//     return 0;
+// }
+
+int main()
+{
+    pid_t parent_pid = getpid(); // 获取父进程的PID
+
+    printf("Parent process PID: %d\n", parent_pid);
+
+    // 创建一个子进程
+    pid_t child_pid = fork();
+
+    if (child_pid < 0)
+    {
+        perror("Fork failed");
+        return 1;
+    }
+    else if (child_pid == 0)
+    {
+        // 这是子进程的代码块
+        printf("This is the child process. PID: %d\n", getpid());
+
+        // 模拟子进程的终止
+        printf("Child process is terminating.\n");
+
+        // 子进程终止
+        _exit(0);
+    }
+    else
+    {
+        // 这是父进程的代码块
+        printf("This is the parent process. Child PID: %d\n", child_pid);
+
+        // 等待子进程终止并获取其退出状态
+        int status;
+        wait(&status);
+
+        printf("Child process has terminated. Parent process is also terminating.\n");
+    }
+
     return 0;
 }
