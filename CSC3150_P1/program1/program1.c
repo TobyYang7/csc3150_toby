a732e803866c1dcb0b8fe9a41ce2ba36952ab170
TobyYang7 <tobyyang7@foxmail.com>
2023-10-06 02:52:37 +0800

    tut2

--- CSC3150_P1/program1/program1.c
+++ CSC3150_P1/program1/program1.c
@@ -5,15 +5,31 @@
 #include <sys/types.h>
 #include <signal.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
 	/* fork a child process */
-	
-	/* execute test program */ 
-	
+	pid_t pid;
+	int status;
+
+	printf("Process start to fork\n");
+	pid = fork();
+
+	if (pid == -1) // error occurred
+	{
+		perror("fork");
+		exit(1);
+	}
+	/* execute test program */
+	else
+	{
+		if (pid == 0)
+		{
+			print("I'm the Child Process, my pid = %d\n", getpid());
+		}
+	}
+
 	/* wait for child process terminates */
-	
+
 	/* check child process'  termination status */
-	
-	
 }
--- Tut2/example_codes/example4.c
+++ Tut2/example_codes/example4.c
@@ -5,31 +5,40 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/**
+ * This program demonstrates the use of fork() system call to create a child process.
+ * The child process prints its process ID and parent process ID, while the parent process waits for the child process to finish and prints the exit status of the child process.
+ *
+ * @param argc The number of arguments passed to the program.
+ * @param argv An array of pointers to the arguments passed to the program.
+ * @return 0 on successful execution.
+ */
 int main(int argc, char *argv[])
 {
     pid_t pid;
     int state;
     printf("Start to fork\n");
-    pid = fork();
-    if (pid == -1)
+    pid = fork();  // create a child process
+    if (pid == -1) // fork() failed
     {
         perror("error");
         exit(1);
     }
     else
     {
-        if (pid == 0)
+        if (pid == 0) // child process
         {
-            printf("I am the Child Process"); // print child process pid
-            sleep(2);
-            printf("Child process pid:%d, ppid:%d\n", getpid(), getppid());
+            printf("I am the Child Process\n");                             // print child process pid
+            sleep(2);                                                       // sleep for 2 seconds
+            printf("Child process pid:%d, ppid:%d\n", getpid(), getppid()); // print child process pid and parent process pid
+            exit(0);                                                        // exit child process
         }
-        else
+        else // parent process
         {
-            waitpid(pid, &state, WNOHANG);
-            printf("Parent process pid:%d\n", getpid());
-            printf("Children process exit with state:%d\n", state);
-            exit(0);
+            waitpid(pid, &state, 0);                                // wait for child process to finish
+            printf("Parent process pid:%d\n", getpid());            // print parent process pid
+            printf("Children process exit with state:%d\n", state); // print child process exit status
+            exit(0);                                                // exit parent process
         }
     }
     return 0;
