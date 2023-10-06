baf02f329642f4d1e25c426790fee8a6f346658e
TobyYang7 <tobyyang7@foxmail.com>
2023-10-06 21:16:40 +0800

    p2

---
+++ CSC3150_P1/program2/program2.mod.c
@@ -0,0 +1,40 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x73708991, "module_layout" },
+	{ 0xe558e54c, "wake_up_process" },
+	{ 0x85602f54, "kthread_create_on_node" },
+	{ 0x9f49dcc4, "__stack_chk_fail" },
+	{ 0xa998009e, "put_pid" },
+	{ 0x26f4e62d, "find_get_pid" },
+	{ 0x952664c5, "do_exit" },
+	{ 0xa7eedcc4, "call_usermodehelper" },
+	{ 0xc5850110, "printk" },
+};
+
+MODULE_INFO(depends, "");
+
