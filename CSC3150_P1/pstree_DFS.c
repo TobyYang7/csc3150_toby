027c3031331bfbe6e3da15e2372d35f9aded4cd8
TobyYang7 <tobyyang7@foxmail.com>
2023-10-08 20:45:43 +0800

    finish

--- CSC3150_P1/pstree_DFS.c
+++ CSC3150_P1/pstree_DFS.c
@@ -6,25 +6,38 @@
 
 #define MAX_NAME_LEN 256
 
+typedef struct
+{
+    pid_t pid;
+    char name[MAX_NAME_LEN];
+} ChildProcess;
+
+int compareChildProcess(const void *a, const void *b)
+{
+    return strcmp(((ChildProcess *)a)->name, ((ChildProcess *)b)->name);
+}
+
 void printProcessTreeDFS(pid_t pid, int depth, int is_last_child, int *sibling_map)
 {
     char proc_name[MAX_NAME_LEN] = "Unknown";
+    char cmd_line[MAX_NAME_LEN] = "";
 
-    // Construct the path to the status file
     char status_path[MAX_NAME_LEN];
     snprintf(status_path, sizeof(status_path), "/proc/%ld/status", (long)pid);
 
-    // Open the status file
     FILE *status_file = fopen(status_path, "r");
     if (status_file)
     {
         char line[MAX_NAME_LEN];
-        // Read the status file to get the process name
         while (fgets(line, sizeof(line), status_file))
         {
             if (sscanf(line, "Name: %s", proc_name) == 1)
             {
-                break;
+                continue;
+            }
+            if (sscanf(line, "Cmdline: %s", cmd_line) == 1)
+            {
+                continue;
             }
         }
         fclose(status_file);
@@ -46,20 +59,23 @@ void printProcessTreeDFS(pid_t pid, int depth, int is_last_child, int *sibling_m
     {
         if (is_last_child)
         {
-            printf("└───");
+            printf("└─");
             sibling_map[depth - 1] = 0;
         }
         else
         {
-            printf("├───");
+            printf("├─");
             sibling_map[depth - 1] = 1;
         }
     }
 
-    // Print the process name
-    printf("%s\n", proc_name);
+    // Print the process name and command line (if available)
+    printf("%s", proc_name);
 
-    // Open the /proc directory
+    // todo: print
+
+    printf("\n");
+
     DIR *dir = opendir("/proc");
     if (!dir)
     {
@@ -68,13 +84,11 @@ void printProcessTreeDFS(pid_t pid, int depth, int is_last_child, int *sibling_m
     }
 
     struct dirent *entry;
-    pid_t children[MAX_NAME_LEN]; // Temporary array to store child processes
-    int child_count = 0;          // Counter for child processes
+    ChildProcess children[MAX_NAME_LEN];
+    int child_count = 0;
 
-    // Iterate through the /proc directory
     while ((entry = readdir(dir)))
     {
-        // Check if the entry is a directory and its name is a number (process ID)
         if (entry->d_type == DT_DIR && atoi(entry->d_name))
         {
             char status_path[MAX_NAME_LEN];
@@ -86,40 +100,39 @@ void printProcessTreeDFS(pid_t pid, int depth, int is_last_child, int *sibling_m
             }
 
             char line[MAX_NAME_LEN];
+            char child_name[MAX_NAME_LEN] = "Unknown";
             int ppid = -1;
-            // Read the status file to get the parent PID
             while (fgets(line, sizeof(line), status_file))
             {
-                if (sscanf(line, "PPid: %d", &ppid) == 1)
-                {
-                    break;
-                }
+                sscanf(line, "Name: %s", child_name);
+                sscanf(line, "PPid: %d", &ppid);
             }
 
             fclose(status_file);
 
-            // If the parent PID matches the current process, add it to the temporary array
             if (ppid == pid)
             {
-                children[child_count++] = atoi(entry->d_name);
+                children[child_count].pid = atoi(entry->d_name);
+                strncpy(children[child_count].name, child_name, MAX_NAME_LEN);
+                child_count++;
             }
         }
     }
 
     closedir(dir);
 
-    // Recursively call the function for each child process
+    qsort(children, child_count, sizeof(ChildProcess), compareChildProcess);
+
     for (int i = 0; i < child_count; i++)
     {
-        printProcessTreeDFS(children[i], depth + 1, i == child_count - 1, sibling_map);
+        printProcessTreeDFS(children[i].pid, depth + 1, i == child_count - 1, sibling_map);
     }
 }
 
 int main()
 {
     printf("pstree:\n");
-    int sibling_map[MAX_NAME_LEN] = {0}; // To keep track of which levels have siblings
-    // Start DFS with the init process (PID 1) and depth 0
+    int sibling_map[MAX_NAME_LEN] = {0};
     printProcessTreeDFS(1, 0, 1, sibling_map);
     return 0;
 }
